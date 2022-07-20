38fdcce94135304b69f20ea65f6c3f711f7f8f11
ChrisW-priv <chris.watras@gmail.com>
2022-07-20 16:15:50 +0200

    creation of different networks with different seeds implemented

--- src/generate_random_conections.py
+++ src/generate_random_conections.py
@@ -1,12 +1,15 @@
 import networkx as nx
 
-n_nodes = 10_000
 
+def make_relations(text_file: str, m_defining_network: int, n_nodes: int, seed=0):
+    graph = nx.barabasi_albert_graph(n_nodes, m_defining_network, seed=seed)
 
-def make_relations(text_file, m):
-    graph = nx.barabasi_albert_graph(n_nodes, m)
+    ''' 
+    HERE YOU CAN ADD THE GROUPING PART 
+    IF YOU DO IT LATER YOU MIGHT FIND THAT THERE WILL BE PROBLEMS WITH MISMATCHING DATA!!!
+    '''
 
-    edges = [edge for edge in graph.edges]
+    edges = list(graph.edges)
     relations = []
 
     # add all relations given by .edges property
@@ -24,8 +27,24 @@ def make_relations(text_file, m):
             new.write(f"{a} {b}\n")
 
 
-m_epidemic = 10
-m_opinion = 10
+def create_random_networks_with_different_seeds(name: str,
+                                                m_defining_network: int,
+                                                n_nodes: int,
+                                                number_of_networks: int = 5):
 
-make_relations("../data/who_knows_who.txt", m_opinion)
-make_relations("../data/who_meets_who.txt", m_epidemic)
+    """creates a number of networks all different from another based on different seed values"""
+
+    for n in range(number_of_networks):
+        filename = f"../data/{name}{n+1}.txt"
+        print('creating', filename)
+        make_relations(filename, m_defining_network, n_nodes, seed=n)
+
+
+if __name__ == '__main__':
+    m_epidemic = 10
+    m_opinion = 10
+    n_nodes = 10_000
+    n_networks = 5
+
+    create_random_networks_with_different_seeds("who_knows_who", m_opinion, n_nodes, n_networks)
+    create_random_networks_with_different_seeds("who_meets_who", m_epidemic, n_nodes, n_networks)
